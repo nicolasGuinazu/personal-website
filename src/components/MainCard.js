22f377c1b26cdcdc23b0ad9307687cb58cef3e0f
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-07-06 16:12:37 -0300

    removed the usage of context in main card

--- src/components/MainCard.js
+++ src/components/MainCard.js
@@ -1,13 +1,13 @@
-import { useContext } from 'react';
+
 import classes from './MainCard.module.css'
-import Contexto from './store/contexto';
+
 
 const MainCard = (props) => {
-  const ctx=useContext(Contexto)
-  return (
-          <div className={classes.maincard} onClick={ctx.onShow}>
   
-            {ctx.show? 'hola':'chau'}
+  return (
+          <div className={classes.maincard} >
+  
+          
           </div>
   )
 
