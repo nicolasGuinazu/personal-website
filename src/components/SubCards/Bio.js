a7e2033c3632294e06c77e12d0366fb00e703747
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-07-07 10:42:02 -0300

    moved bio component to subcards, wrote a personal description

---
+++ src/components/SubCards/Bio.js
@@ -0,0 +1,25 @@
+import classes from './Bio.module.css'
+import selff from './selff.jpg'
+const Bio = (props) => {
+    return (
+      <>
+      <section>
+        <article className={classes.bio}>
+        <img src={selff}/>
+        <div className={classes.texto}>
+        <h1>Hola soy Nicolas!</h1>
+        <p>Desde el 2019 inicie mi camino en el apasionante mundo de la informatica y el desarrollo web</p>
+        <p> Soy Licenciado en turismo y actualmente estudio informatica en la Universisad Nacional de La Plata</p>
+        <p>El compromiso y la dedicacion son las bases que definen mi desempeno profesional </p>
+       
+        </div>
+        
+        </article>
+     
+      </section>
+        
+      </>
+    );
+  };
+  
+  export default Bio;
