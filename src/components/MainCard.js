de0b9767d0403b25f2d23018429e8d89fa763f7a
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-07-07 09:42:54 -0300

    adjusted the css styles, just initial phase tho

--- src/components/MainCard.js
+++ src/components/MainCard.js
@@ -1,13 +1,14 @@
 
+
 import classes from './MainCard.module.css'
-
-
+import ProgLag from './SubCards/ProgLag';
+import Languages from './SubCards/Languages';
 const MainCard = (props) => {
   
   return (
           <div className={classes.maincard} >
-  
-          
+            {/* <ProgLag/> */}
+            <Languages/>
           </div>
   )
 
