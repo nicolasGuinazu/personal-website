329cd597cfa3767d6cec506c6e9f44d88f592b7e
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-07-08 11:14:49 -0300

    changed the stlye a bit and included the education component

--- src/components/MainCard.js
+++ src/components/MainCard.js
@@ -2,6 +2,7 @@
 
 import classes from './MainCard.module.css'
 import ProgLag from './SubCards/ProgLag';
+import Education from './SubCards/Education';
 import Languages from './SubCards/Languages';
 import Bio from './SubCards/Bio';
 import { useContext } from 'react';
@@ -10,6 +11,7 @@ const MainCard = (props) => {
   const ctx=useContext(Contexto)
   return (
           <div className={classes.maincard} >
+            {ctx.showEdu && <Education/>} 
             {ctx.showProg && <ProgLag/>} 
             {ctx.showLang && <Languages/>}
             {ctx.showBio && <Bio/>}
