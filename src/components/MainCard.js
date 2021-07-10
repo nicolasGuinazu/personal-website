b7d8833fbdc91c18848524743b2960fecd60a0d1
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-07-10 10:25:43 -0300

    included the experience component and added logic from ctx

--- src/components/MainCard.js
+++ src/components/MainCard.js
@@ -4,6 +4,7 @@ import classes from './MainCard.module.css'
 import ProgLag from './SubCards/ProgLag';
 import Education from './SubCards/Education';
 import Languages from './SubCards/Languages';
+import Exper from './SubCards/Exper';
 import Bio from './SubCards/Bio';
 import { useContext } from 'react';
 import Contexto from './store/contexto';
@@ -14,6 +15,7 @@ const MainCard = (props) => {
             {ctx.showEdu && <Education/>} 
             {ctx.showProg && <ProgLag/>} 
             {ctx.showLang && <Languages/>}
+            {ctx.showJob && <Exper/>}
             {ctx.showBio && <Bio/>}
           </div>
   )
