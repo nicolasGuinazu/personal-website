62585eba87d557e6c5b9c80f1c45564d5a490ebc
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-07-10 10:26:33 -0300

    created the job experience component, styling similar to bio

---
+++ src/components/SubCards/Exper.js
@@ -0,0 +1,29 @@
+import classes from './Exper.module.css'
+const Exper = (props) => {
+  return (
+    <>
+      <section>
+        <article className={classes.experience}>
+
+          <p> (2018 -2020) DOCENTE ADSCRIPTO DE INVESTIGACION Y AYUDANTE DE SEGUNDA.</p>
+          <p>Facultad de Ciencias Económicas
+            y Sociales. Universidad Nacional
+            de Mar del Plata</p>
+          <p>(2018 -2019) INFORMANTE TURISTICO.
+            Ente Muinicipal de Turismo Mar del Plata</p>
+          <p>(2013 -2019) SECRETARIA Y ACREDITACIONES.
+            G2 Consultora MCI Argentina</p>
+          <q> XV World Congress of Comparative Education Society</q>
+          <q>VI Latin American Symposium on Maternal-Foetal Interaction and Placenta</q>
+          <q> LVIII - LXIV Annual meetings of the Argentine Society of Clinical Investigation.</q>
+          <q> Congreso Iberoamericano de Hormigón Elaborado</q>
+     
+        </article>
+
+      </section>
+
+    </>
+  );
+};
+
+export default Exper;
