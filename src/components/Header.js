ad83141d96eccf1b01526021865da7c0b702d99b
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-07-12 09:41:37 -0300

    added a hamburger icon for mobile, the idea is to place the buttons there

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,13 @@ import Switch from '@material-ui/core/Switch';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import { useContext } from 'react';
 import Contexto from './store/contexto';
+
 const Header = (props) => {
   const ctx=useContext(Contexto)
   return (
     <>
       <header className={classes.header}>
+        <div className={classes.ham}><i class="fas fa-bars fa-lg"></i></div>
         
         <h1 className={classes.title}>Nicolas Guinazu</h1>
         
