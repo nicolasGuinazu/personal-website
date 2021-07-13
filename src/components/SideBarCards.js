7d9144397a8ada38e74c83f5df2a468bb64e6cf6
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-07-13 09:34:15 -0300

    created sidebar component due to conflicts with css styling

---
+++ src/components/SideBarCards.js
@@ -0,0 +1,25 @@
+import SchoolSharpIcon from '@material-ui/icons/SchoolSharp';
+import CodeIcon from '@material-ui/icons/Code';
+import WorkIcon from '@material-ui/icons/Work';
+import TranslateIcon from '@material-ui/icons/Translate';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import Card from './Card'
+import classes from './SideBarCards.module.css'
+import Contexto from './store/contexto';
+import { useContext } from 'react';
+
+const SideBarCards = (props) => {
+  const ctx=useContext(Contexto)
+  return (
+    <div className={classes.sideBarCards}>
+      
+      <Card><SchoolSharpIcon fontSize="large" className={classes.icon}onClick={ctx.onShowEdu} /></Card>
+      <Card><CodeIcon fontSize="large" className={classes.icon} onClick={ctx.onShowProg}/></Card>
+      <Card><TranslateIcon fontSize="large" className={classes.icon} onClick={ctx.onShowLang}/></Card> 
+      <Card><WorkIcon fontSize="large" className={classes.icon} onClick={ctx.onShowJob}/></Card>
+      <Card><AccountCircleIcon fontSize="large" className={classes.icon} onClick={ctx.onShowBio}/></Card>
+    </div>
+  );
+};
+
+export default SideBarCards;
