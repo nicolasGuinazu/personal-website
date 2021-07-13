c9ee40a6f816bda731f0ae4aa36f1a1c465506b2
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-07-13 09:37:24 -0300

    used state to create a toggler to show or hide sidebar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,20 @@
 import classes from './Header.module.css'
 import Switch from '@material-ui/core/Switch';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import Contexto from './store/contexto';
-
+import SideBarCards from './SideBarCards';
 const Header = (props) => {
   const ctx=useContext(Contexto)
+  const [show,setShow]=useState(false)
+  const showToggle=()=>{
+    setShow(!show)
+  }
   return (
     <>
       <header className={classes.header}>
-        <div className={classes.ham}><i class="fas fa-bars fa-lg"></i></div>
-        
+        <div className={classes.ham} onClick={showToggle}><i class="fas fa-bars fa-lg"></i></div>
+        {show ? <SideBarCards/> : ''}
         <h1 className={classes.title}>Nicolas Guinazu</h1>
         
         <FormControlLabel
